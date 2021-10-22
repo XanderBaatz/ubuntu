84132795925ac4f518d0e17c5378784f8d28e35e
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-10-22 18:27:53 +0200

    Update deb-sources-list-gen.sh

--- deb-sources-list-gen.sh
+++ deb-sources-list-gen.sh
@@ -15,16 +15,20 @@ sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
 
 # sources.list generation
 sudo sh -c "cat << EOF > /etc/apt/sources.list
-deb ${repo_url} stable ${component}
-deb-src ${repo_url} stable ${component}
+deb ${repo_url} ${code_name} ${component}
+deb-src ${repo_url} ${code_name} ${component}
 
-deb http://security.debian.org/ stable-security ${component}
-deb-src http://security.debian.org/ stable-security ${component}
+deb ${repo_url} ${code_name}-updates ${component}
+deb-src ${repo_url} ${code_name}-updates ${component}
 
-deb ${repo_url} stable-updates ${component}
-deb-src ${repo_url} stable-updates ${component}
+deb ${repo_url} ${code_name}-security ${component}
+deb-src ${repo_url} ${code_name}-security ${component}
 
 deb ${repo_url} ${code_name}-backports ${component}
+deb-src ${repo_url} ${code_name}-backports ${component}
+
+deb http://archive.canonical.com/ubuntu ${code_name} partner
+deb-src http://archive.canonical.com/ubuntu ${code_name} partner
 EOF"
 
 # update sources
