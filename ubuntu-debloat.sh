e896fdec9a4d8298514874362adbb5a9edd24323
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-10-26 17:41:32 +0200

    Update ubuntu-debloat.sh

--- ubuntu-debloat.sh
+++ ubuntu-debloat.sh
@@ -2,7 +2,8 @@
 # wget -qO- https://git.io/JiliQ | sudo sh
 
 #delete all packages except for generic kernel, grub, ubuntu-minimal...
-e_pkgs="linux-generic*|grub*|ubuntu-minimal|ubuntu-standard"
+e_pkgs="linux-generic*|grub*|ubuntu-minimal"
 
-sudo apt-mark auto $(apt list --installed | cut -f1 -d'/' | grep -v "^Listing" | grep -E -v "linux-generic*|grub*|ubuntu-minimal|ubuntu-standard") && sudo apt autoremove --purge -y && sudo apt autoclean all -y
+sudo apt-mark auto $(apt list --installed | cut -f1 -d'/' | grep -v "^Listing" | grep -E -v "linux-generic*|grub*|ubuntu-minimal") && sudo apt autoremove --purge -y && sudo apt autoclean all -y
+sudo apt install ubuntu-standard -y
 sudo update-grub
