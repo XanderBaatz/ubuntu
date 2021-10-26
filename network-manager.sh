143075c8698136d9243fcc42a27c8232615b8e44
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-10-26 18:02:02 +0200

    Create network-manager.sh

---
+++ network-manager.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+sudo apt install -y network-manager --no-install-recommends
+
+sudo systemctl disable systemd-networkd.{service,socket}
+sudo systemctl mask systemd-networkd.{service,socket}
+sudo systemctl stop systemd-networkd.{service,socket}
+
+sudo cp /etc/netplan/*.yaml $(ls /etc/netplan/*.yaml).bak
+sudo rm -rf /etc/netplan/*.yaml
+
+sudo sh -c 'cat << EOF > /etc/netplan/01-network-manager-all.yaml
+network:
+  version: 2
+  renderer: NetworkManager
+EOF'
+
+# setup netplan for NM
+sudo netplan generate
+sudo netplan apply
+# make sure NM is running
+sudo systemctl enable NetworkManager.service
+sudo systemctl restart NetworkManager.service
