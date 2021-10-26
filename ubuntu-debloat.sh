af800084376e855c1df613f858fbd1c2c7d99795
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-10-26 17:32:42 +0200

    Update ubuntu-debloat.sh

--- ubuntu-debloat.sh
+++ ubuntu-debloat.sh
@@ -4,4 +4,4 @@
 #delete all packages except for generic kernel, grub, ubuntu-minimal...
 e_pkgs="linux-generic*|grub*|ubuntu-minimal|ubuntu-standard"
 
-sudo sh -c "apt-mark auto $(apt list --installed | cut -f1 -d'/' | grep -v "^Listing" | grep -E -v "${e_pkgs}") -y && apt autoremove --purge -y && apt autoclean all -y"
+sudo sh -c "apt-mark auto $(apt list --installed | cut -f1 -d'/' | grep -v "^Listing" | grep -E -v "linux-generic*|grub*|ubuntu-minimal|ubuntu-standard") -y && apt autoremove --purge -y && apt autoclean all -y"
