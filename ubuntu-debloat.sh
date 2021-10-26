3c4d5243d53142da0f1d48b4355d51e89ec196c7
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-10-26 17:22:22 +0200

    Update ubuntu-debloat.sh

--- ubuntu-debloat.sh
+++ ubuntu-debloat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#remove cloud-init
-#remove ubuntu-server
-#remove snapd
+#delete all packages except for generic kernel, grub, ubuntu-minimal
+sudo apt-mark auto $(apt list --installed | cut -f1 -d'/' | grep -v "^Listing" | grep -E -v "linux-generic*|grub*|ubuntu-minimal") -y && \
+sudo apt autoremove --purge -y && apt autoclean all -y
