e6142489154d07dfd91e12632684eb84bb13f10e
XanderBaatz <35949906+XanderBaatz@users.noreply.github.com>
2021-10-26 17:55:08 +0200

    Update ubuntu-debloat.sh

--- ubuntu-debloat.sh
+++ ubuntu-debloat.sh
@@ -10,5 +10,6 @@ fi
 e_pkgs="linux-generic*|grub*|ubuntu-minimal"
 
 sudo apt-mark auto $(apt list --installed | cut -f1 -d'/' | grep -v "^Listing" | grep -E -v "linux-generic*|grub*|ubuntu-minimal") && sudo apt autoremove --purge -y && sudo apt autoclean all -y
+sudo apt update -y && sudo apt upgrade -y
 sudo apt install ubuntu-standard -y
 sudo update-grub
